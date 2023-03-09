5eb29cb7bee8bb9efbda6aa212d813517aef9274
Papa Babacar Ngor SENGHOR <papa-babacar-ngor.senghor@atos.net>
2023-03-09 20:47:15 +0100

    Initial commit

---
+++ src/main/java/sn/iler/sn/iler/paiement/Application.java
@@ -0,0 +1,13 @@
+package sn.iler.sn.iler.paiement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/sn/iler/sn/iler/paiement/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.iler.sn.iler.paiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
